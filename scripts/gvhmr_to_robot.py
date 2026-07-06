8b3bdf5214a0167b1a7352a50f5a96f06d4365aa
lim-0219 <joan0219@yonsei.ac.kr>
2026-07-06 10:03:11 +0900

    Add: kapex retargeting code 2

--- scripts/gvhmr_to_robot.py
+++ scripts/gvhmr_to_robot.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     
     parser.add_argument(
         "--robot",
-        choices=["unitree_g1", "unitree_h1"],
+        choices=["unitree_g1", "unitree_h1", "kapex", "unitree_go2"],
         default="unitree_g1",
     )
     
